1ad3160b64b51d15ee22647e33950d8f84fad70c
Adrian Kuma <kuma.adrian@gmail.com>
2017-01-25 21:31:29 +0100

    Changed server address

--- src/app/main.js
+++ src/app/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-window.socket = window.io.connect('http://127.0.0.1:4666');
+window.socket = window.io.connect();
 
 console.log('Przypinamy do ');
 console.log(document.getElementById('app'));
