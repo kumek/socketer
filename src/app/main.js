37a70d2923205f24cc7ff5487f54f96a2fa3735e
Adrian Kuma <kuma.adrian@gmail.com>
2017-02-01 22:28:54 +0100

    Legacy fix

--- src/app/main.js
+++ src/app/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-window.socket = window.io.connect('http://localhost:4666');
-
 console.log('Przypinamy do ');
 console.log(document.getElementById('app'));
 ReactDOM.render(<App />, document.getElementById('app'));
